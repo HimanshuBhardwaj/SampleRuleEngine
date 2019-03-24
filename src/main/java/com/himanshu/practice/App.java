33f5c4de146aa00dd2acdde303c565c87be066f2
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2019-03-24 16:46:48 +0530

    irst commit

---
+++ src/main/java/com/himanshu/practice/App.java
@@ -0,0 +1,13 @@
+package com.himanshu.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
